7d802e88a366dd8747c91dce4eccad8e2573b478
ganeshmahadev <ganeshmahadev2463@gmail.com>
2025-08-06 02:39:59 +0200

    model evaluation and mlflow for experiment tracking

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
+import mlflow
 
 
 class ModelTrainer:
@@ -32,6 +33,17 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
          
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+            
+            mlflow.sklearn.log_model(best_model, "model")
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision_score", precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
                 "Random Forest": RandomForestClassifier(verbose=1),
@@ -83,10 +95,12 @@ class ModelTrainer:
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
-
+        self.track_mlflow(best_model,classification_train_metric)
+        logging.info(f"Best model found on both training and testing dataset: {best_model_name} with score: {best_model_score}")
+        
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
-
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
